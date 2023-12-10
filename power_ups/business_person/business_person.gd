97d49b9ff69ee782445b147950604a84986c6cbb
Josh@40AU <josh.jensen@fortyau.com>
2023-12-09 19:25:31 -0600

    adjusted animation for business person

--- power_ups/business_person/business_person.gd
+++ power_ups/business_person/business_person.gd
@@ -1,8 +1,9 @@
 extends RigidBody2D
-
+signal time_expired
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Timer.start()
 	pass # Replace with function body.
 
 
@@ -12,6 +13,7 @@ func _process(delta):
 
 
 func _on_timer_timeout():
+	time_expired.emit()
 	queue_free()
 	
 func get_mob_type():
